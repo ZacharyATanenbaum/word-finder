99f6b05afb5f8362d76edb7cca4ac13fafa4d104
ZacharyATanenbaum <zachtanenbaum@gmail.com>
2018-12-09 19:48:37 -0500

    Updated index.php to explain Word-Finder

--- index.php
+++ index.php
@@ -25,43 +25,34 @@
         <?php include $base."leftMenuGame.php"; ?>
     </nav>
     <article>
-        <h1 id="gameName">The Rubber Band Game</h1>
-        <h3 id="groupName">Team Zorro</h3>
+        <h1 id="gameName">Word-Finder</h1>
+        <h3 id="groupName">The Raging Dissapointments</h3>
         <h3>Instruction:</h3>
         <div id="gameDesc" class="jumbotron">
             <div>
                 <p>
                     <b>How to Win</b>
                     <br>
-                    <font size="2">The objective of the game is to control more nails that your opponent. You gain control of nails by encircling them with rubber bands. Nails that do not touch a rubber band will belong to the player whose rubber band encloses the nail. If both players' bands enclose a nail, the nail belongs to the player whose band is closer to the nail.</font>
+                    <font size="2">The goal of the game is to have the most points when the game ends. The game ends when all rounds have been completed. A round being a cycle where each player gets a single turn.</font>
                 </p>
                 <p>
                     <b>Turns</b>
                     <br>
-                    <font size="2">Both players play alternately, placing rubber bands on the nails. Players start with 5 bands by default. This number can be changed by making a selection in the settings menu (accesible by the button with the cog icon).</font>
+                    <font size="2">A turn is split into two section - The Action Phase and Word Phase. In the Action Phase a player gets to rotate a column (up/down) a row (right/left) or swap a letter with any other letter. The Word Phase is where a player may select a word on the board to remove for points. The removed letters are then re-populated randomly.</font>
                 </p>
                 <p>
                     <b>Rules</b>
                     <br>
                 <ul>
-                    <li>You can select a maximum of 4 nails. The minimum is 1</li>
-                    <li>You may not place a rubber band on a nail that already has another band touching it</li>
-                    <li>Selection order matters. You can only draw convex shapes with the band. Concave shapes are not allowed</li>
-                    <li>Any invalid selection is discarded. You may also reselect nails if you wish to change your current selection</li>
+                    <li>A word may only go from left to right or up to down.</li>
+                    <li>A word must be adjacent letters that are not diagonal.</li>
+                    <li>The maximum board size is 12 letters by 12 letters.</li>
+                    <li>There may be up to four players and a bot. His name is Botty Mc. BotFace.</li> 
                 </ul>
                 </p>
             </div>
         </div>
         <div id="scoreArea", class="jumbotron">
-	<?php
-	    include $base."getScore.php";
-	    /*
-	    * arg1: gameName, should be the same as the dir name
-	    * arg2: if your score is sortable, pass 1 if higher score is better, 0
-	    *       if smaller score is better. Otherwise no need to pass variable
-	    */
-	    getScore("Rubberband", 1);
-	?>
 	</div>
         <h3>Play It!</h3>
         <form id="gameSettings" class="well">
